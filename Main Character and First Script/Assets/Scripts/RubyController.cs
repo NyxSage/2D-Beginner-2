4bca7af37fbac6b217c6854e87c2d976c049c354
NyxSage <53097710+NyxSage@users.noreply.github.com>
2021-10-19 13:59:17 -0500

    World Design

--- Main Character and First Script/Assets/Scripts/RubyController.cs
+++ Main Character and First Script/Assets/Scripts/RubyController.cs
@@ -7,14 +7,26 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Retrieves Ruby's curent position so that those values can be changed
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+
+        //Alters Ruby's position depending on the keys pressed
+        //independently of a systems frames per second
+
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //Stores Ruby's new position
+
         transform.position = position;
     }
 }
